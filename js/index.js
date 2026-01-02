cb1b4b8dd146a8dfe27331662ffc9ca53e69b065
Mateo781 <mateo.silvero.t1vl@gmail.com>
2026-01-02 18:32:48 -0300

    Pagina casi personalizada

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@
         btnMaps.addEventListener('click', (e) => {
             e.preventDefault();
             // Reemplaza esta URL con la de tu local en Google Maps
-            window.open('https://maps.google.com/?q=Tu+Direccion+Aqui', '_blank');
+            window.open('https://www.google.com/maps/place/El+Duende+-+Pizzería+y+Casa+de+Comidas/@-34.5084373,-58.5739057,3a,75y,174.09h,82.25t/data=!3m7!1e1!3m5!1szukc6uuzZeMHej5MdKp05Q!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fcb_client%3Dmaps_sv.tactile%26w%3D900%26h%3D600%26pitch%3D7.753381294935949%26panoid%3Dzukc6uuzZeMHej5MdKp05Q%26yaw%3D174.0862870656139!7i16384!8i8192!4m14!1m7!3m6!1s0x95bcba8b9b44dfb3:0xa2039dcdf66dcbd6!2sEl+Duende+-+Pizzería+y+Casa+de+Comidas!8m2!3d-34.5086161!4d-58.5739454!16s%2Fg%2F11by_crnj8!3m5!1s0x95bcba8b9b44dfb3:0xa2039dcdf66dcbd6!8m2!3d-34.5086161!4d-58.5739454!16s%2Fg%2F11by_crnj8?entry=ttu&g_ep=EgoyMDI1MTIwOS4wIKXMDSoASAFQAw%3D%3D', '_blank');
         });
 
         // Botón menú completo (aquí puedes agregar la funcionalidad que necesites)
